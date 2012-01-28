903a0719a5d47d7e87db7a7e283c22ee90f5cbc6
Vignesh Sarma <vignesh.sarma@gmail.com>
2012-01-28 20:56:09 +0530

    Added a few changes and Readme

--- prime/prime.rb
+++ prime/prime.rb
@@ -5,27 +5,24 @@ class Prime
   def initialize(limit='')
     @limit=limit.to_i
     @sum=0
+    @allPrimes=[]
     (2..@limit).each do |num|
-      if is_prime?(num)
+      if prime?(num)
         @sum+=num
+        @allPrimes.push num
+        puts num
       end
     end
     puts @sum
   end
-  def is_prime?(n)
-    return true if n == 2
-    return false if n & 1 == 0 
-    # runs the block for each number from 2 to sqrt(n)
-    # only returns true if the block *never* returns true
-    (3..(Math.sqrt(n).to_i)).step(2).none? { |i| n%i == 0 }
-  end
 
-  def isPrime?(number)
+  def prime?(number)
     limit=Math.sqrt(number)
     if number===1 or number === 0 or limit=== Integer(limit)
       return false
     end
-    (2..limit).each do |check|
+    @allPrimes.each do |check|
+      return true if check >limit
       if number%check===0
         return false
       end
--- prime/prime_spec.rb
+++ prime/prime_spec.rb
@@ -6,25 +6,22 @@ describe Prime do
     prime=Prime.new "10"
     #prime.limit.should eq(10)
     prime.sum.should eq(17)
-    prime1=Prime.new "20"
+    prime1=Prime.new "534"
     #prime1.limit.should eq(20)
+    #prime1.sum.should eq(23592)
+    #prime1=Prime.new "2497236"
   end
+  
   it "isPrime should check whether the given int is prime or not" do
     prime=Prime.new "10"    
-    prime.is_prime?(5).should eq(true)
-    prime.is_prime?(2).should eq(true)
-    prime.is_prime?(3).should eq(true)
-    prime.is_prime?(4).should eq(false)
-    prime.is_prime?(9).should eq(false)
-    #prime.is_prime?(1).should eq(false)
-    #prime.is_prime?(0).should eq(false)
-    prime.isPrime?(5).should eq(true)
-    prime.isPrime?(2).should eq(true)
-    prime.isPrime?(3).should eq(true)
-    prime.isPrime?(4).should eq(false)
-    prime.isPrime?(9).should eq(false)
-    prime.isPrime?(1).should eq(false)
-    prime.isPrime?(0).should eq(false)
+
+    prime.prime?(5).should eq(true)
+    prime.prime?(2).should eq(true)
+    prime.prime?(3).should eq(true)
+    prime.prime?(4).should eq(false)
+    prime.prime?(9).should eq(false)
+    prime.prime?(1).should eq(false)
+    prime.prime?(0).should eq(false)
   end
 
   
